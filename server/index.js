7bd45b3b6c415ca5d4d22e287d0e8e78968a6204
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2021-05-05 04:34:57 -0400

    refactoring

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ const __dirname = dirname(__filename);
 
 if (process.env.NODE_ENV === 'production') {
     // Serve any static files
-    server.use(express.static(path.join(__dirname, '../client/build')));
+    server.use(express.static(__dirname, '../client/build'));
 
     // Handle React routing, return all requests to React app
     server.get('/', function(req, res) {
