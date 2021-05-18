bb98f381facf407e3d1838d458d129a231568754
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2021-05-18 17:35:11 -0400

    added search functionality,installed lodash.debounce

--- client/src/components/RecordsList/RecordsList.js
+++ client/src/components/RecordsList/RecordsList.js
@@ -1,10 +1,13 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import  axios  from 'axios';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
+import debounce from "lodash.debounce";
 
 function RecordsList(props) {
-  const [records, setRecords] = useState([])
+  const [records, setRecords] = useState([]);
+  const [query, setQuery] = useState("");
+
 
 
   useEffect(() => {
@@ -19,6 +22,22 @@ function RecordsList(props) {
     getRecords();
   }, []);
 
+  let filteredRecords = records;
+
+  if (query !== "") {
+    filteredRecords = records.filter((record) => {
+      return record.last_name.toLowerCase().includes(query.toLowerCase()) || record.first_name.toLowerCase().includes(query.toLowerCase()) || record.city.toLowerCase().includes(query.toLowerCase()) || record.state.toLowerCase().includes(query.toLowerCase()) ;
+    });
+  }
+
+  const changeHandler = (e) => {
+    setQuery(e.target.value);
+  };
+
+  const debouncedChangeHandler = useMemo(() => {
+    return debounce(changeHandler, 300);
+  }, []);
+
 
   return (
     <div>
@@ -27,14 +46,17 @@ function RecordsList(props) {
         <Link to="/records/new" className="btn btn-primary float-right">New Record</Link> 
       </h2>
       <hr/>
-      {records
+       <div>
+      <input
+        onChange={debouncedChangeHandler}
+        type="text"
+        placeholder="Search records..."
+      />
+      {filteredRecords
         .sort((a,b)=> {
             return b.date > a.date ? 1: -1
-        })
-        .map((record) => {
-        return(
-        
-          <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-evenly" ,listStyleType: "none", paddingRight: "10px" }}key={record._id}>
+        }).map((record) => (
+        <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-evenly" ,listStyleType: "none", paddingRight: "10px" }}key={record._id}>
             <li>{record.first_name}</li>
             <li>{record.last_name}</li>
             <li>{record.address1}</li>
@@ -45,9 +67,10 @@ function RecordsList(props) {
             <li>{record.country}</li>
             <li>{moment(record.date).toString()}</li>
             <hr/>
-          </div>
-        )     
-      })}
+        </div>
+      ))}
+      <div>{filteredRecords.length === 0 && query !== "" && "No matches found..."}</div> 
+     </div> 
     </div>
   )
 }
