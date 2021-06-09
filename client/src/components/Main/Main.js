afb62056cae59100eec58566684803d0be264211
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2021-06-09 18:23:40 -0400

    minor refactoring

--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -8,10 +8,10 @@ import RecordAdd from '../Form/Form';
 function Main() {
     return(
       <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/records" component={RecordsList} />
         <Route exact path="/records/new" component={RecordAdd} />
+        <Route exact path="/records" component={RecordsList} />
         <Route exact path="/success" component={RecordSuccess} />
+        <Route exact path="/" component={Home} />
       </Switch>
     );
   }
