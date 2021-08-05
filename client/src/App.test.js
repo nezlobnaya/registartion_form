6fc55f4858dce5ec305e7d468169d0e3a14881b1
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2021-08-05 17:02:34 -0400

    Added tests to App.test.js

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,39 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('Per Aspera Ad Astra!!', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Per Aspera Ad Astra!!/i);
-  expect(linkElement).toBeInTheDocument();
+describe('<App />', () => {
+  test('should render', () => {
+    const { getByText } = render(<App />);
+    const app = getByText('Per Aspera Ad Astra!!');
+    expect(app).toBeInTheDocument();
+  });
+
+  test('Per Aspera Ad Astra!!', () => {
+    render(<App />);
+    const linkElement = screen.getByText(/Per Aspera Ad Astra!!/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test('snapshot for App component', () => {
+    const { container } = render(<App />);
+    expect(container).toMatchSnapshot();
+  });
+
+  test('snapshot for App component with custom className', () => {
+    const { container } = render(<App className="custom" />);
+    expect(container).toMatchSnapshot();
+  });
+
+  test('snapshot for App component with custom style', () => {
+    const { container } = render(<App style={{ background: 'red' }} />);
+    expect(container).toMatchSnapshot();
+  } );
+
+  test('snapshot for App component with custom className and style', () => {  
+    const { container } = render(<App className="custom" style={{ background: 'red' }} />);
+    expect(container).toMatchSnapshot();
+  } );
 });
+
+
+
