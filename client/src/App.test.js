16cdb146e31be4440fc21d2ab802c3da91e25477
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2021-08-05 16:36:05 -0400

    minor test fix

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Per Aspera Ad Astra!!', () => {
   render(<App />);
   const linkElement = screen.getByText(/Per Aspera Ad Astra!!/i);
   expect(linkElement).toBeInTheDocument();
