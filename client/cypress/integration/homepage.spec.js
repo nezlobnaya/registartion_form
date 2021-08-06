eff0f28cccd605576613a43c778e3f240064dc59
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2021-08-05 20:21:11 -0400

    cypress test refactoring

--- client/cypress/integration/homepage.spec.js
+++ client/cypress/integration/homepage.spec.js
@@ -1,6 +1,9 @@
 describe("renders the home page", () => {
     it("renders the home page", () => {
         cy.visit("/");
-        cy.contains("Per Aspera Ad Astra!!");
+        cy.contains("Per Aspera Ad Astra!!")
+        cy.contains("Home")
+        cy.contains("Admin report")
+        cy.contains("New Record");
     })
 })
