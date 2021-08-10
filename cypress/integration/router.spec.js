b00250d63289f5822a2cb325ff6e55b9f7d958da
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2021-08-09 20:20:54 -0400

    more cypress tests to test endpoints added

--- cypress/integration/router.spec.js
+++ cypress/integration/router.spec.js
@@ -24,7 +24,29 @@ describe('http example', () => {
         }).then(response => {
             expect(response.status).to.eq(201);
             expect(response.body).to.have.property('message', 'Record created successfully!')})
-
     })
+
+    it('PUT',function() {
+        cy.request('PATCH', 'http://localhost:8000/api/records/6111c32b91fa9e5d04c185f2', {
+            first_name: 'test',
+            last_name: 'test',
+            '_id': '6111c32b91fa9e5d04c185f2',
+                address1: '730 West 178',
+                address2: '',
+                city: 'CCC',
+                state: 'UT',
+                zip: '84606',
+                country: 'US',
+        }).then(response => {
+            expect(response.status).to.eq(200);
+            expect(response.body).to.have.property('city', 'CCC')})
+})
+
+    it('DELETE',function() {
+        cy.request('DELETE', 'http://localhost:8000/api/records/6111c32b91fa9e5d04c185f2').then(response => {
+            expect(response.body).to.have.property('message', 'Record deleted successfully.')})
+    })
+
+    
 });
 
