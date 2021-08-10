93569abb15bbd92f4f1e30cbdb4b7962adf1903c
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2021-08-09 20:07:38 -0400

    added some more cypress tests to test endpoints

--- cypress/integration/router.spec.js
+++ cypress/integration/router.spec.js
@@ -6,8 +6,25 @@ describe('http example', () => {
         })
         .then(response => {
             expect(response.status).to.eq(200);
+            expect(response.body).have.property('length');
+            assert.notExists(null, 'val is null or undefined')
         });
     })
 
+    it('POST',function() {
+        cy.request('POST', 'http://localhost:8000/api/records', {
+            first_name: 'test',
+            last_name: 'test',
+                address1: '730 West 178',
+                address2: '',
+                city: 'SLC',
+                state: 'UT',
+                zip: '84606',
+                country: 'US',
+        }).then(response => {
+            expect(response.status).to.eq(201);
+            expect(response.body).to.have.property('message', 'Record created successfully!')})
+
+    })
 });
 
