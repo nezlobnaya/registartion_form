2d6556ecf934783df438b628b304c9534d59e04c
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2021-08-20 15:58:51 -0400

    minor cleanup

--- client/src/auth/auth0-provider-with-history.js
+++ client/src/auth/auth0-provider-with-history.js
@@ -13,7 +13,7 @@ const Auth0ProviderWithHistory = ({ children }) => {
   const onRedirectCallback = (appState) => {
     history.push(appState?.returnTo || window.location.pathname);
   };
-  console.log(window.location.origin)
+
   return (
     <Auth0Provider
       domain={domain}
