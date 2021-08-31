dafb64504f100a44efc6559b28e67275811adf66
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2021-08-31 19:39:34 -0400

    added conditional rendering to Profile

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -1,8 +1,10 @@
 import { NavLink } from 'react-router-dom';
 import AuthNav from "./auth-nav";
+import { useAuth0 } from '@auth0/auth0-react';
 
 
 function Navigation() {
+  const { isAuthenticated } = useAuth0();
     return(
       <nav className="navbar sticky-top navbar-expand-lg navbar-light mb-4" style={{backgroundColor: "#e3f2fd"}}>
         <div className='container'>
@@ -10,7 +12,7 @@ function Navigation() {
             <li className="nav-item font-weight-normal"><NavLink exact className="nav-link" activeClassName="active" to="/">Home</NavLink></li>
             <li className="nav-item font-weight-normal"><NavLink exact className="nav-link" activeClassName="active" to="/records/">Admin report</NavLink></li>
             <li className="nav-item font-weight-normal"><NavLink exact className="nav-link" activeClassName="active" to="/records/new">New Record</NavLink></li>
-            <li className="nav-item font-weight-normal"><NavLink exact className="nav-link" activeClassName="active" to="/profile">Profile</NavLink></li>
+            {isAuthenticated && <li className="nav-item font-weight-normal"><NavLink exact className="nav-link" activeClassName="active" to="/profile">Profile</NavLink></li>}
           </ul>
           <AuthNav />
         </div>
